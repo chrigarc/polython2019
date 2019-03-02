55898e953e46834921956a05326d2331f451f39b
Christian Garcia <chrigarc@ciencias.unam.mx>
2019-03-02 13:00:04 -0600

    Polymer 3 init

---
+++ src/polython2019-app/polython2019-app.js
@@ -0,0 +1,45 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@polymer/app-route/app-route';
+import '@polymer/app-route/app-location';
+import '@polymer/iron-pages/iron-pages';
+import '../pages/landing-page';
+import '../pages/login-page';
+/**
+ * @customElement
+ * @polymer
+ */
+class Polython2019App extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <h2>Hello [[prop1]]!</h2>
+      <app-location route="{{route}}"></app-location>
+      <app-route route="{{route}}"
+                   pattern="/:page"
+                   data="{{data}}"></app-route>
+        <iron-pages
+                selected="{{data.page}}"
+                attr-for-selected="view"
+                fallback-selection="landing"
+                role="main">
+            <landing-page view="landing"></landing-page>
+            <login-page view="login"></login-page>
+        </iron-pages>
+
+    `;
+  }
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'polython2019-app'
+      }
+    };
+  }
+}
+
+window.customElements.define('polython2019-app', Polython2019App);
